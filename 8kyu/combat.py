0549240b6c763e975498a8f6ed6edb1db83fbbab
mugishaj092 <mugishajoseph092@gmail.com>
2026-02-02 11:15:25 +0200

    Add combat function to calculate new health after damage

---
+++ 8kyu/combat.py
@@ -0,0 +1,7 @@
+def combat(health, damage):
+    return max(health-damage,0)
+
+
+'''
+Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+'''
