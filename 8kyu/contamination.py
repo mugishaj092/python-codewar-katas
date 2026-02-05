cb47ffe74d5d2861ffffbef5b083bec9a60fa55a
mugishaj092 <mugishajoseph092@gmaill.com>
2026-02-05 08:50:43 +0200

    Add contamination function to replace characters in a string

---
+++ 8kyu/contamination.py
@@ -0,0 +1,19 @@
+def contamination(text, char):
+    if not text or not char:
+        return ""
+    return char * len(text)
+
+
+'''
+Starting with the original text, and given a character, return the text once it has been mutated so that all of the characters in the original text have been replaced with the character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+'''
